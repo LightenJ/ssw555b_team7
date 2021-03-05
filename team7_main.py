3800e55fb6f1d2c59698aeae5f68491c5108a081
Emay-Pandarakutty <79270064+Emay-Pandarakutty@users.noreply.github.com>
2021-03-04 19:57:21 -0500

    updated the parser

--- team7_main.py
+++ team7_main.py
@@ -19,14 +19,10 @@ After reading all of the data, print the unique identifiers and names of each of
 
 import sys
 from prettytable import PrettyTable
-
+from datetime import datetime
 import validity_test
-import gedcom
 from gedcom.parser import Parser
 from gedcom.element.individual import IndividualElement
-
-# welcome Message
-print('welcome to P03 Python Assignment\n')
 
 print('Enter file name with extension when prompted  e.g : test.ged \n')
 
@@ -50,13 +46,48 @@ Tag_Level = {
     'TRLR': 0,
     'NOTE': 0}
 
-ID = []
-Name = []
-Fam = []
-Hus = []
-Wif = []
+
 Ind = PrettyTable(["ID", "NAME"])
-Family = PrettyTable(["ID", "Husband Name", "Wife Name"])
+Fam = PrettyTable(["ID", "Husband Name", "Wife Name"])
+ind = []
+fam = []
+ind_list = []
+individuals = []
+families = []
+read_dates = []
+
+class Individual:
+    def __init__(self, ind_id):
+        self.ind_id = ind_id
+        self.name = None
+        self.sex = None
+        self.birth_d = None
+        self.death_d = None
+        self.spouse_id = None
+        self.child_id = None
+
+class Family:
+    def __init__(self, fam_id):
+        self.fam_id = fam_id
+        self.marriage_d = None
+        self.hus_id = None
+        self.wife_id = None
+        self.children = []
+        self.divorce_d = None
+
+def date_before(dates):
+
+    valid = False
+    try:
+        for date in dates:
+            if date != None:
+                f_date = datetime.strptime(date.rstrip(), '%d %b %Y').date()
+                c_date = datetime.now().date()
+                if f_date > c_date:
+                    valid = True
+    except: valid = None
+
+    return valid
 
 def data_match(splitline):
     data_found = False
@@ -69,75 +100,139 @@ def data_match(splitline):
             break
     return data_found, index
 
+def strip_valid_line(line):
+    splitline = []
+    line = line.strip()  # Return a copy of the sequence with specified leading and trailing bytes removed (spaces)
+    if len(line) > 1:  # ignored any data without 2 parameters.
+        splitline = line.split(' ', 2)
+        found, index = data_match(splitline)
+        if index != 1:  # swapping 2 and 3 index if the tag is present in 3 element
+            splitline[1], splitline[2] = splitline[2], splitline[1]
+        if len(splitline) > 2:
+            if '@' in splitline[2]:
+                splitline[2] = splitline[2].replace("@", "")
+    return splitline
 
-def find_str(fhand):
+def extract_ind_date (instance, day, splitline):
+    if day == 'BIRT':
+        instance.birth_d = splitline[2]
+    if day == 'DEAT':
+        instance.death_d = splitline[2]
+
+def extract_mar_date(instance, day, splitline):
+    if day == 'MARR':
+        instance.marriage_d = splitline[2]
+    if day == 'DIV':
+        instance.divorce_d = splitline[2]
+
+def extract_individual_info(read_lines, line_no, individual):
+    line_no +=1
+    splitline = strip_valid_line(read_lines[line_no])
+    while splitline[0] != "0" and line_no < len(read_lines):
+        if splitline[1] == 'NAME':
+            individual.name = splitline[2]
+
+        if splitline[1] == 'SEX':
+            individual.sex = splitline[2]
+
+        if splitline[1] == 'BIRT' or splitline[1] == 'DEAT':
+            extract_ind_date(individual, splitline[1], strip_valid_line(read_lines[line_no+1]))
+
+        if splitline[1] == 'FAMC':
+            individual.child_id = splitline[2]
+
+        if splitline[1] == 'FAMS':
+            individual.spouse_id = splitline[2]
+
+        line_no +=1
+        splitline = strip_valid_line(read_lines[line_no])
+    individuals.append(individual)
+
+def extract_family_info(read_lines, line_no, family):
+    line_no +=1
+    splitline = strip_valid_line(read_lines[line_no])
+    while splitline[0] != "0" and line_no < len(read_lines):
+
+        if splitline[1] == 'MARR' or splitline[1] == 'DIV':
+            extract_mar_date(family, splitline[1], strip_valid_line(read_lines[line_no+1]))
+
+        if splitline[1] == 'HUSB':
+            family.hus_id = splitline[2]
+
+        if splitline[1] == 'WIFE':
+            family.wife_id = splitline[2]
+
+        if splitline[1] == 'CHIL':
+            family.children = splitline[2]
+
+        line_no +=1
+        splitline = strip_valid_line(read_lines[line_no])
+
+    families.append(family)
+
+def find_str(read_lines):
     """
     To extract the valve and update the variable value only if more than 2 parameters are there to check with the provided dict
     """
-    global ID, Name, Ind, Fam, Hus, Wif
-    ind_list = []
-    for line in fhand:
-        line = line.strip()  # Return a copy of the sequence with specified leading and trailing bytes removed (spaces)
-        if len(line) > 1:  # ignored any data without 2 parameters.
-            splitline = line.split(' ', 2)
-            found, index = data_match(splitline)
-            if index != 1:  # swapping 2 and 3 index if the tag is present in 3 element
-                splitline[1], splitline[2] = splitline[2], splitline[1]
-            if len(splitline) > 2:
-                if '@' in splitline[2]:
-                    splitline[2] = splitline[2].replace("@", "")
+    line_no = 0
+    while line_no < len(read_lines):
+        splitline = strip_valid_line(read_lines[line_no])
 
-            if splitline[0] == '0' and splitline[1] == 'INDI':
-                if splitline[2] not in ID:
-                    ID.append(splitline[2])
+        if splitline[0] == '0' and splitline[1] == 'INDI':
+            extract_individual_info(read_lines, line_no, Individual(splitline[2]))
 
-            if splitline[0] == '1' and splitline[1] == 'NAME':
-                if splitline[2] not in Name:
-                    Name.append(splitline[2])
+        if splitline[0] == '0' and splitline[1] == 'FAM':
+            extract_family_info(read_lines, line_no, Family(splitline[2]))
+        line_no += 1
 
-            if splitline[0] == '0' and splitline[1] == 'FAM':
-                if splitline[2] not in Fam:
-                    Fam.append(splitline[2])
 
-            if splitline[0] == '1' and splitline[1] == 'HUSB':
-                if splitline[2] not in Hus:
-                    Hus.append(splitline[2])
-
-            if splitline[0] == '1' and splitline[1] == 'WIFE':
-                if splitline[2] not in Wif:
-                    Wif.append(splitline[2])
-
-    for i, n in zip(ID, Name):
-        Ind.add_row([i, n])
-        ind_list.append((i, n))
+    for ind in individuals:
+        Ind.add_row([ind.ind_id, ind.name])
+        ind_list.append((ind.ind_id, ind.name))
+        read_dates.append(ind.birth_d)
+        read_dates.append(ind.death_d)
     Ind.sortby = 'ID'
 
     print("Individual ID and Name \n", Ind)
 
-    for f, h, w in zip(Fam, Hus, Wif):
-        hus_name = ''#default
-        wif_name = ''#default
+    for f in families:
+        f_id = f.fam_id
+        read_dates.append(f.marriage_d)
+        read_dates.append(f.divorce_d)
+
+        hus_name = '' #default
+        wif_name = '' #default
         for i in range(len(ind_list)):
-            if ind_list[i][0] == h:
+            if ind_list[i][0] == f.hus_id:
                 hus_name = ind_list[i][1]
-            if ind_list[i][0] == w:
+            if ind_list[i][0] == f.wife_id:
                 wif_name = ind_list[i][1]
 
-        Family.add_row([f, hus_name, wif_name])
+        Fam.add_row([f_id, hus_name, wif_name])
 
-    Family.sortby = 'ID'
+    Fam.sortby = 'ID'
 
-    print(" Family info \n", Family)
+    print(" Family info \n", Fam)
+
+    date_before(read_dates)
 
 
 fname = input('Enter the file name: ')
 try:
     fhand = open(fname)  # open File
+    read_lines = fhand.readlines()
 except:
     print('File cannot be opened:', fname)
     sys.exit()
 
 sys.stdout = open('OutputFile.txt', 'w')
+
+
+try:
+    find_str(read_lines)
+    fhand.close()  # Close the file
+except:
+    print('Processing failure')
 
 try:
     gedcom_parser = Parser()
@@ -156,6 +251,5 @@ try:
     fhand.close()  # Close the file
 except:
     print('Processing failure')
-
 
 
