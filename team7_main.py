a0a303eff9a716550376c9d54b83e5f07ebce81e
Emay-Pandarakutty <79270064+Emay-Pandarakutty@users.noreply.github.com>
2021-03-27 16:35:33 -0400

    Update team7_main.py
    Minor name update

--- team7_main.py
+++ team7_main.py
@@ -212,7 +212,7 @@ try:
         print(error)
     print("\nUS22 ==> ALL IDs Unique ?:", validity_test.unique_ids(ids_list))
     print("\nUS23 ==> Duplicate name and birth date:", validity_test.unique_name_and_birth_date(individuals))
-    print("\nUS24 ==> Unique families by spouses:", validity_test.unique_families_by_spouses(individuals,families))
+    print("\nUS24 ==> Duplicate families by spouses:", validity_test.unique_families_by_spouses(individuals,families))
     print("\nUS30 ==> List of living married is : \n", validity_test.list_of_living_married(individuals))
     print("\nUS31 ==> List of living single is : \n", validity_test.list_of_living_single(individuals))
     print("\nUS35 ==>List of recent births : \n", validity_test.list_of_recent_births(read_birth_dates,individuals))
