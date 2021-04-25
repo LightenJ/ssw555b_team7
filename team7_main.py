f186b0eb03df2da138abfa6e9840a609fb1aa81d
sureshmurali710 <54928199+sureshmurali710@users.noreply.github.com>
2021-04-25 10:23:56 -0400

    Update team7_main.py

--- team7_main.py
+++ team7_main.py
@@ -232,7 +232,7 @@ try:
     print("\nUS32 ==> List of Multiple births in a family : \n",validity_test.List_multiple_births(individuals, families))
     print("\nUS16 ==> Male last names : \n", validity_test.us16_male_last_names(individuals, families))
     print("\nUS42 ==> Illegitimate dates : \n", validity_test.us42_reject_illegitimate_dates(i, f))                  
-    print("\nUS41 ==> Accept partial dates : \n", validity_test.us41_accept_partial_dates(individuals, families))    
+    print("\nUS41 ==> Accept partial dates : \n", validity_test.us41_accept_partial_dates(individuals))    
 except Exception as exception:
     print(exception)
 
